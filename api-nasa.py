8fde330b82c75759bc331fe2a00552002e12254b
Sam <samdeoliveira@protonmail.com>
2023-04-25 18:40:44 -0300

    Update api-nasa.py

--- api-nasa.py
+++ api-nasa.py
@@ -1,5 +1,6 @@
 
 import requests
+import re
 
 
 R = '\u001b[31m';
@@ -14,6 +15,12 @@ print(f'''
 
 ''')
 
-url = "https://api.nasa.gov/planetary/apod?api_key=6mhKfETfzsKDfHnuJauVVoWwEyz65gPOVhLEnhml"
-response = requests.get(url)
-print(response.json())
+response = requests.get('https://api.nasa.gov/planetary/apod?api_key=6mhKfETfzsKDfHnuJauVVoWwEyz65gPOVhLEnhml')
+data = response.json()
+link_regex = re.compile(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+
+for key, value in data.items():
+    if isinstance(value, str) and link_regex.search(value):
+        print(value)
+
+
